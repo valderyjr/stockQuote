ab17c312e6fad1d865f6ace41ee51a1cfcc6e1ef
valderyjr <valderyjunior2002@gmail.com>
2023-08-20 19:45:22 -0300

    feat: create project

---
+++ StockQuote/Program.cs
@@ -0,0 +1,17 @@
+﻿
+namespace StockQuote
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isFormatted = args.Length == 3;
+            if (!isFormatted)
+            {
+                Console.WriteLine("Formato incorreto. Você deve seguir o seguinte formato: NOME_DA_ACAO PRECO_DE_COMPRA PRECO_DE_VENDA");
+                return;
+            };
+
+        }
+    }
+}
