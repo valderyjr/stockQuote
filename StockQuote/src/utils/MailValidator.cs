d7ced6dfdc41970896635366b75a642e53268270
valderyjr <valderyjunior2002@gmail.com>
2023-08-28 20:42:48 -0300

    feat: create MailValidator

---
+++ StockQuote/src/utils/MailValidator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace StockQuote.Utils
+{
+    public static class MailValidator
+    {
+
+        private static string _mailValidatorRegex = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+        public static bool IsValid(string mail)
+        {
+            if (string.IsNullOrWhiteSpace(mail)) return false;
+
+            try
+            {
+                return Regex.IsMatch(mail,
+                    _mailValidatorRegex,
+                    RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
+            }
+
+        }
+    }
+}
